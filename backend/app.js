fb1388869c098c71b19cea21e8baeef46092e0a4
Isabel Andrade <isabelandrade965@gmail.com>
2026-03-03 17:14:54 -0300

    fix: corrigir app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); 
 
 const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(cors());
 
 // FRONTEND (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
